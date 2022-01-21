24ca78a5b67225fc0e53bc9592366857dace6546
André Felipe Dias <andre.dias@pronus.io>
2022-01-21 10:24:40 -0300

    make line_length configurable and include github repo url

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,12 +3,19 @@
 poetry update
 if [ "{{cookiecutter.version_control}}" == "hg" ]; then
     hg init .
+    if [ -n "{{cookiecutter.github_respository_url}}" ]; then
+        echo '[paths]
+default = {{cookiecutter.github_respository_url}}
+' > .hg/hgrc
+    fi
     hg add
     hg commit -m 'Initial project structure'
 else
     mv .hgignore .gitignore
     git init .
-    sed -i 1d .gitignore
+    if [ -n "{{cookiecutter.github_respository_url}}" ]; then
+        git remote add origin {{cookiecutter.github_respository_url}}
+    fi
     git add -A .
     git commit -m 'Initial project structure'
 fi
