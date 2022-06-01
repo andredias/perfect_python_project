e5f150940dea027507050e54f1177f2d6a798458
André Felipe Dias <andre.dias@pronus.io>
2022-05-31 21:28:27 -0300

    fix smoke test check for hello message

--- {{cookiecutter.project_slug}}/scripts/smoke_test.py
+++ {{cookiecutter.project_slug}}/scripts/smoke_test.py
@@ -7,34 +7,33 @@ from httpx import get
 
 
 def run_smoke_test() -> None:
+    # fmt: off
     check_call(
         [
             'docker-compose',
-            '-f',
-            'docker-compose.yml',
-            '-f',
-            'docker-compose.smoke_test.yml',
-            'up',
-            '-d',
+            '-f', 'docker-compose.yml',
+            '-f', 'docker-compose.smoke_test.yml',
+            'up', '-d',
         ]
     )
+    # fmt: on
     sleep(2)
     try:
         result = get('http://localhost:5000/hello')
         assert result.status_code == 200
-        assert result.json() == {'Hello': 'World'}
+        assert result.json() == {'message': 'Hello World'}
         print('Smoke test passed!')
     finally:
+        # fmt: off
         check_call(
             [
                 'docker-compose',
-                '-f',
-                'docker-compose.yml',
-                '-f',
-                'docker-compose.smoke_test.yml',
+                '-f', 'docker-compose.yml',
+                '-f', 'docker-compose.smoke_test.yml',
                 'down',
             ]
         )
+        # fmt: on
 
 
 if __name__ == '__main__':
