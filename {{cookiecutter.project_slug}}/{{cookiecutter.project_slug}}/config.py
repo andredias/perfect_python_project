27fe3ad13d8371fabb4128010b78993379815b7a
André Felipe Dias <andre.dias@pronus.io>
2022-06-29 18:12:57 -0300

    declare type annotations for environment variables in config.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ENV = os.getenv('ENV', 'production').lower()
+ENV: str = os.getenv('ENV', 'production').lower()
 if ENV not in ('production', 'development', 'testing'):
     raise ValueError(
         f'ENV={ENV} is not valid. '
         "It should be 'production', 'development' or 'testing'"
     )
-DEBUG = ENV != 'production'
-TESTING = ENV == 'testing'
+DEBUG: bool = ENV != 'production'
+TESTING: bool = ENV == 'testing'
 
-LOG_LEVEL = os.getenv('LOG_LEVEL') or (DEBUG and 'DEBUG') or 'INFO'
+LOG_LEVEL: str = os.getenv('LOG_LEVEL') or (DEBUG and 'DEBUG') or 'INFO'
 os.environ['LOGURU_DEBUG_COLOR'] = '<fg #777>'
