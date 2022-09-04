96cb7dc8d9a5ffe3f2589764854a2649165114cb
André Felipe Dias <andre.dias@pronus.io>
2022-09-04 09:59:33 -0300

    startup and shutdown events declared as parameters in FastAPI app instantiation

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -13,18 +13,10 @@ app = FastAPI(
     title='{{cookiecutter.project_name}}',
     debug=config.DEBUG,
     default_response_class=ORJSONResponse,
+    on_startup=[startup],
+    on_shutdown=[shutdown],
 )
 
 
 for router in routers:
     app.include_router(router)
-
-
-@app.on_event('startup')
-async def startup_event() -> None:
-    await startup()
-
-
-@app.on_event('shutdown')
-async def shutdown_event() -> None:
-    await shutdown()
