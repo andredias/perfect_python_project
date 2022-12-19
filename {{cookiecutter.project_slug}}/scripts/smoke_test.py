d84f430b0ce103eff9624bb1289dbbe37529f689
André Felipe Dias <andre.dias@pronus.io>
2022-12-19 02:57:44 -0300

    smoke_test improved

--- {{cookiecutter.project_slug}}/scripts/smoke_test.py
+++ {{cookiecutter.project_slug}}/scripts/smoke_test.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 
 from subprocess import check_call
-from time import sleep
 
 from httpx import get
+from loguru import logger
+from tenacity import RetryError, retry, stop_after_delay, wait_exponential
+
+
+def init_containers() -> None:
+    command = 'docker-compose -f docker-compose.yml -f docker-compose.smoke_test.yml up -d'
+    check_call(command.split())
+
+
+def stop_containers() -> None:
+    command = 'docker-compose -f docker-compose.yml -f docker-compose.smoke_test.yml down'
+    check_call(command.split())
+
+
+@retry(stop=stop_after_delay(3), wait=wait_exponential(multiplier=0.2))
+def health_check() -> None:
+    result = get('http://localhost:5000/hello')
+    assert result.status_code == 200
+    assert result.json() == {'message': 'Hello World'}
+    logger.info('Smoke test passed!')
+    return
 
 
 def run_smoke_test() -> None:
-    # fmt: off
-    check_call(
-        [
-            'docker-compose',
-            '-f', 'docker-compose.yml',
-            '-f', 'docker-compose.smoke_test.yml',
-            'up', '-d',
-        ]
-    )
-    # fmt: on
-    sleep(2)
+    init_containers()
     try:
-        result = get('http://localhost:5000/hello')
-        assert result.status_code == 200
-        assert result.json() == {'message': 'Hello World'}
-        print('Smoke test passed!')  # noqa: T201
+        health_check()
+    except RetryError:
+        logger.error('Not working')
     finally:
-        # fmt: off
-        check_call(
-            [
-                'docker-compose',
-                '-f', 'docker-compose.yml',
-                '-f', 'docker-compose.smoke_test.yml',
-                'down',
-            ]
-        )
-        # fmt: on
+        stop_containers()
 
 
 if __name__ == '__main__':
