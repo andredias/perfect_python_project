dba6d559409aac95e6e5e8144c1ee771d6f29bb0
André Felipe Dias <andre.dias@pronus.io>
2023-01-22 08:29:29 -0300

    linting should be done in pre-push hook

--- {{cookiecutter.project_slug}}/scripts/install_hooks.sh
+++ {{cookiecutter.project_slug}}/scripts/install_hooks.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
 
-GIT_PRE_COMMIT='#!/bin/bash
-cd $(git rev-parse --show-toplevel)
-poetry run make lint
-'
-
 GIT_PRE_PUSH='#!/bin/bash
 cd $(git rev-parse --show-toplevel)
-poetry run make test
+poetry run make lint && poetry run make test
 '
 
 HG_HOOKS='[hooks]
-precommit.lint = (cd `hg root`; poetry run make lint)
-pre-push.test = (cd `hg root`; poetry run make test)
+pre-push.lint_test = (cd `hg root`; poetry run make lint && poetry run make test)
 '
 
 if [ -d '.git' ]; then
-    echo "$GIT_PRE_COMMIT" > .git/hooks/pre-commit
     echo "$GIT_PRE_PUSH" > .git/hooks/pre-push
     chmod +x .git/hooks/pre-*
-elif ! grep -s -q 'precommit.lint' '.hg/hgrc'; then
+elif ! grep -s -q 'pre-push.lint_test' '.hg/hgrc'; then
     echo "$HG_HOOKS" >> .hg/hgrc
 fi
