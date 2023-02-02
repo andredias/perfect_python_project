248e9f2d12f5ab7736da66c3e92b2a4e21118fe3
André Felipe Dias <andre.dias@pronus.io>
2023-02-01 21:37:46 -0300

    alembic should be included into the Docker image

---
+++ {{cookiecutter.project_slug}}/entrypoint.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+from pathlib import Path
+from subprocess import run
+
+from {{cookiecutter.project_slug}} import config
+from {{cookiecutter.project_slug}}.resources import connect_database, Database
+
+parent_path = Path(__file__).parent
+
+
+async def migrate() -> None:
+    """
+    Wait for the database to be ready.
+    """
+    db = Database(config.DATABASE_URL)
+    await connect_database(db)
+    await db.disconnect()
+    run('alembic upgrade head'.split(), cwd=parent_path)
+    return
+
+
+async def main() -> None:
+    if os.getenv('ENV') != 'production':
+        await migrate()
+
+    run('hypercorn --config=hypercorn.toml {{cookiecutter.project_slug}}.main:app'.split(), cwd=parent_path)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
