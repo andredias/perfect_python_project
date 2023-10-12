b1c81ad276a3125fdd4300788b97fe9a26065875
André Felipe Dias <andre.dias@pronus.io>
2023-10-12 13:02:00 -0300

    change the request id generation to use secrets.token_urlsafe instead of uuid4

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/middleware.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/middleware.py
@@ -1,10 +1,12 @@
-import time
-import uuid
 from collections.abc import Callable
+from secrets import token_urlsafe
+from time import time
 
 from fastapi import Request, Response
 from hypercorn.logging import AccessLogAtoms
 from loguru import logger
+
+from . import config
 
 
 async def log_request_middleware(request: Request, call_next: Callable) -> Response:
@@ -12,13 +14,13 @@ async def log_request_middleware(request: Request, call_next: Callable) -> Respo
     This middleware will log all requests and their processing time.
     """
     # Create a request ID
-    request_id = str(uuid.uuid4())
+    request_id: str = token_urlsafe(config.REQUEST_ID_LENGTH)
 
     # Add context to all loggers in all views
-    start_time = time.time()
+    start_time = time()
     with logger.contextualize(request_id=request_id):
         response = await call_next(request)
-        final_time = time.time()
+        final_time = time()
         elapsed = final_time - start_time
         response_dict = {
             'status': response.status_code,
