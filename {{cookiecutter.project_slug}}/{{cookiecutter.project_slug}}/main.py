ecd700e08a4eea54e7298be8683cc0a0ee662073
André Felipe Dias <andre.dias@pronus.io>
2023-10-21 18:34:50 -0300

    Add middleware based on BaseHTTPMiddleware instead of using middleware decorator

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from . import config  # noqa: F401
 from .exception_handlers import request_validation_exception_handler
 from .resources import lifespan
 from .routers import hello
-from .middleware import generic_exception_handler, log_request_middleware
+from .middleware import log_request_middleware
 
 app = FastAPI(
     title='{{cookiecutter.project_name}}',
@@ -19,6 +20,5 @@ routers = (
 for router in routers:
     app.include_router(router)
 
-app.middleware('http')(generic_exception_handler)
-app.middleware('http')(log_request_middleware)
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_request_middleware)
 app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
