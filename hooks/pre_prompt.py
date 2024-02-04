111263546af90fa583442085b3f1916e6d9acd07
André Felipe Dias <andre.dias@pronus.io>
2024-02-04 13:08:41 -0300

    select worker_class (uvloop or asyncio) for hypercorn based on operational system

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,8 +1,10 @@
+import os
 import json
 import sys
 from pathlib import Path
 
 config = Path("cookiecutter.json")
 data = json.loads(config.read_text())
+data["worker_class"] = "uvloop" if os.name == "posix" else "asyncio"
 data["python_version"] = f"{sys.version_info.major}.{sys.version_info.minor}"
 config.write_text(json.dumps(data, indent=4))
