c5d9ce1ddf2297217202763cf1ba41c7f7706dfb
André Felipe Dias <andref.dias@gmail.com>
2025-11-20 11:08:45 -0300

    fix: the htmx app does not use /api as a root path

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -3,10 +3,8 @@ set -eox pipefail
 
 if [ "$ENV" != 'production' ]; then
     exec hypercorn --reload --worker-class={{cookiecutter.worker_class}} --bind=0.0.0.0:5000 \
-                   --error-logfile=- --root-path /api \
-                   {{cookiecutter.project_slug}}.main:app
+                   --error-logfile=- {{cookiecutter.project_slug}}.main:app
 else
     exec hypercorn --worker-class={{cookiecutter.worker_class}} --bind=0.0.0.0:5000 --error-logfile=- \
-                   --error-logfile=- --root-path /api \
-                   {{cookiecutter.project_slug}}.main:app
+                   --error-logfile=- {{cookiecutter.project_slug}}.main:app
 fi
