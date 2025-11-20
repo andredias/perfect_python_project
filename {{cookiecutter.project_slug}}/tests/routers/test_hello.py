dd03916e16280c21cfdc933683a983c5b86f78b9
André Felipe Dias <andref.dias@gmail.com>
2025-11-19 23:45:56 -0300

    fix: router test hello world

--- {{cookiecutter.project_slug}}/tests/routers/test_hello.py
+++ {{cookiecutter.project_slug}}/tests/routers/test_hello.py
@@ -2,6 +2,6 @@ from httpx import AsyncClient
 
 
 async def test_hello(client: AsyncClient) -> None:
-    response = await client.get('/hello')
+    response = await client.get("/fragment/hello")
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World'}
+    assert response.text == "<h1>Hello World!</h1>"
