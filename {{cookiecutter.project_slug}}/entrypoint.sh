e0010a3dc0d0be7921c09694dbd9b5456b0ec54b
André Felipe Dias <andref.dias@gmail.com>
2025-12-29 19:49:38 -0300

    database migration can run independently from development

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -eoux pipefail
 
-if [ "$ENV" != 'production' ]; then
+if [ -n "$MIGRATE_DB" ]; then
     # ref: https://pythonspeed.com/articles/schema-migrations-server-startup/
     python migrate_database.py
+fi
+
+if [ "$ENV" != 'production' ]; then
     exec granian --reload --loop {{cookiecutter.worker_class}} --host 0.0.0.0 --port 5000 \
                  --url-path-prefix /api --interface asgi {{cookiecutter.project_slug}}.main:app
 else
