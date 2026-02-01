2dca70d2eb6e8fd533d68ee9d22251f6a29516b0
André Felipe Dias <andref.dias@gmail.com>
2026-02-01 18:56:30 -0300

    small improvement to exception handling

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/exception_handlers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/exception_handlers.py
@@ -16,6 +16,6 @@ async def request_validation_exception_handler(
     """
     method = request.get('method', 'GET')
     query_string = request['query_string'].decode()
-    path_with_query = request['path'] + ('?' + query_string if query_string else '')
+    path_with_query = f'{request["path"]}?{query_string}' if query_string else request['path']
     logger.info('request validation exception', method=method, path_with_query=path_with_query, detail=exc.errors())
     return await _request_validation_exception_handler(request, exc)
